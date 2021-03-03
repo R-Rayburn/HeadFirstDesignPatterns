093fbbc19dcd05dcec83c1cccbcb473ae51793ef
Robert Rayburn <robert.rayburn@core10.io>
2021-03-02 23:56:05 -0600

    testing out Swing's internal Observer code

---
+++ Chapter2/SwingObserver/src/SwingObserverExample.java
@@ -0,0 +1,33 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class SwingObserverExample {
+    JFrame frame;
+    public static void main(String[] args) {
+        SwingObserverExample example = new SwingObserverExample();
+        example.go();
+    }
+
+    public void go() {
+        frame = new JFrame();
+
+        JButton button = new JButton("Should I do it?");
+        button.addActionListener(new AngelListener);
+        button.addActionListener(new DevilListener);
+
+        // Additional frame properties
+    }
+
+    class AngelListener implements ActionListener {
+        public void actionPerformed(ActionEvent event) {
+            System.out.println("Don't do it, you might regret it!");
+        }
+    }
+
+    class DevilListener implements ActionListener {
+        public void actionPerformed(ActionEvent event) {
+            System.out.println("Come on, do it!");
+        }
+    }
+}
