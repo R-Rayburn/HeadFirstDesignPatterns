c7ffbd18597590ae63aa9b3e4c1c100404429b49
Robert Rayburn <robert.rayburn@core10.io>
2021-03-15 17:39:02 -0500

    cleaned up Beverage class

--- Chapter3/Beverage/src/Beverage.java
+++ Chapter3/Beverage/src/Beverage.java
@@ -1,19 +1,5 @@
 public abstract class Beverage {
     public String description = "Unknown Beverage";
-//    private boolean milk;
-//    private boolean soy;
-//    private boolean mocha;
-//    private boolean whip;
-
     public String getDescription() { return description; }
     public abstract double cost();
-
-//    public boolean hasMilk() { return milk; }
-//    public void setMilk(boolean milk) { this.milk = milk; }
-//    public boolean hasSoy() { return soy; }
-//    public void setSoy(boolean soy) { this.soy = soy; }
-//    public boolean hasMocha() { return mocha; }
-//    public void setMocha(boolean mocha) { this.mocha = mocha; }
-//    public boolean hasWhip() { return whip; }
-//    public void setWhip(boolean whip) { this.whip = whip; }
 }
