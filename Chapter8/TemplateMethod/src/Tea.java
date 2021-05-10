f941f7466be945821689a5b282ea2810c04f86ac
Robert Rayburn <robert.rayburn@core10.io>
2021-05-10 12:16:53 -0500

    adding initial classes for coffee and tea

---
+++ Chapter8/TemplateMethod/src/Tea.java
@@ -0,0 +1,25 @@
+public class Tea {
+
+    void prepareRecipe() {
+        boilWater();
+        steepTeaBag();
+        pourInCup();
+        addLemon();
+    }
+
+    private void addLemon() {
+        System.out.println("Adding lemon");
+    }
+
+    private void pourInCup() {
+        System.out.println("Pouring into cup");
+    }
+
+    private void steepTeaBag() {
+        System.out.println("Steeping the tea");
+    }
+
+    private void boilWater() {
+        System.out.println("Boiling water");
+    }
+}
