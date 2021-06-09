2e9e62a11a255d2e2fffddfeec206c531496ca29
Robert Rayburn <robert.rayburn@core10.io>
2021-06-09 16:11:20 -0500

    updated waitress logic on handling printing out menus

--- Chapter09/Composite/src/Waitress.java
+++ Chapter09/Composite/src/Waitress.java
@@ -1,35 +1,11 @@
-import java.util.Iterator;
-
 public class Waitress {
-    Menu pancakeHouseMenu;
-    Menu dinerMenu;
-    Menu cafeMenu;
+    MenuComponent allMenus;
 
-    public Waitress(Menu pancakeHouseMenu, Menu dinerMenu, Menu cafeMenu) {
-        this.pancakeHouseMenu = pancakeHouseMenu;
-        this.dinerMenu = dinerMenu;
-        this.cafeMenu = cafeMenu;
+    public Waitress(MenuComponent allMenus) {
+        this.allMenus = allMenus;
     }
 
     public void printMenu() {
-        Iterator<MenuItem> pancakeHouseMenuIterator = pancakeHouseMenu.createIterator();
-        Iterator<MenuItem> dinerIterator = dinerMenu.createIterator();
-        Iterator<MenuItem> cafeIterator = cafeMenu.createIterator();
-
-        System.out.println("MENU\n----\nBREAKFAST");
-        printMenu(pancakeHouseMenuIterator);
-        System.out.println("\nLUNCH");
-        printMenu(dinerIterator);
-        System.out.println("\nDINNER");
-        printMenu(cafeIterator);
-    }
-
-    public void printMenu(Iterator<MenuItem> iterator) {
-        while (iterator.hasNext()) {
-            MenuItem menuItem = iterator.next();
-            System.out.print(menuItem.getName() + " ($");
-            System.out.print(menuItem.getPrice() + ") \n\t- ");
-            System.out.println(menuItem.getDescription());
-        }
+        allMenus.print();
     }
 }
