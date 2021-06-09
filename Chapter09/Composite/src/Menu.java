a22d30ba83e0c7f88aacc1972fcd543399a404d0
Robert Rayburn <robert.rayburn@core10.io>
2021-06-09 15:37:12 -0500

    extended MenuComponent into Composite Menu class, converting it from an interface to a class

--- Chapter09/Composite/src/Menu.java
+++ Chapter09/Composite/src/Menu.java
@@ -1,5 +1,32 @@
-import java.util.Iterator;
+import java.util.*;
 
-public interface Menu {
-    Iterator<MenuItem> createIterator();
+public class Menu extends MenuComponent {
+    List<MenuComponent> menuComponents = new ArrayList<>();
+    String name;
+    String description;
+
+    public Menu(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public void add(MenuComponent menuComponent) { this.menuComponents.add(menuComponent); }
+
+    public void remove(MenuComponent menuComponent) { this.menuComponents.remove(menuComponent); }
+
+    public MenuComponent getChild(int i) { return this.menuComponents.get(i); }
+
+    public String getName() { return name; }
+
+    public String getDescription() { return description; }
+
+    public void print() {
+        System.out.println("\n" + getName());
+        System.out.println(getDescription());
+        System.out.println("--------------------");
+
+        for (MenuComponent menuComponent : menuComponents) {
+            menuComponent.print();
+        }
+    }
 }
