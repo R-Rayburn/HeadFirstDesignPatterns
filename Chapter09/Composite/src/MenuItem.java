4a5ca7f6b98fd63b972733a145670743d839825e
Robert Rayburn <robert.rayburn@core10.io>
2021-06-09 15:29:03 -0500

    updated leaf class MenuItem to extend MenuComponent

--- Chapter09/Composite/src/MenuItem.java
+++ Chapter09/Composite/src/MenuItem.java
@@ -1,4 +1,4 @@
-public class MenuItem {
+public class MenuItem extends MenuComponent {
     String name;
     String description;
     boolean vegetarian;
@@ -21,4 +21,11 @@ public class MenuItem {
     public boolean isVegetarian() { return vegetarian; }
 
     public double getPrice() { return price; }
+
+    public void print() {
+        System.out.print(" " + getName());
+        if (isVegetarian()) System.out.print("(v)");
+        System.out.println(" ($" + getPrice() + ")");
+        System.out.println("\t- " + getDescription());
+    }
 }
