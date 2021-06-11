26f3f054c68d7bdced35fcdcd0945541edc0dd2c
Robert Rayburn <robert.rayburn@core10.io>
2021-06-10 23:05:44 -0500

    implemented no quarter state class

--- Chapter10/State/src/NoQuarterState.java
+++ Chapter10/State/src/NoQuarterState.java
@@ -1,17 +1,24 @@
 public class NoQuarterState implements State {
-    public void insertQuarter() {
+    GumballMachine gumballMachine;
 
+    public NoQuarterState(GumballMachine gumballMachine) {
+        this.gumballMachine = gumballMachine;
+    }
+
+    public void insertQuarter() {
+        System.out.println("You inserted a quarter");
+        gumballMachine.setState(gumballMachine.getHasQuarterState());
     }
 
     public void ejectQuarter() {
-
+        System.out.println("You haven't inserted a quarter");
     }
 
     public void turnCrank() {
-
+        System.out.println("You turned, but there is no quarter");
     }
 
     public void dispense() {
-
+        System.out.println("You need to pay in order to get a gumball");
     }
 }
