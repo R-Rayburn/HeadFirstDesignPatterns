ea93311f6e17a2f39a2a1345bf6c471aa9c68b02
Robert Rayburn <robert.rayburn@core10.io>
2021-06-10 23:16:05 -0500

    implemented sold state class

--- Chapter10/State/src/SoldState.java
+++ Chapter10/State/src/SoldState.java
@@ -1,17 +1,29 @@
 public class SoldState implements State {
-    public void insertQuarter() {
+    GumballMachine gumballMachine;
 
+    public SoldState(GumballMachine gumballMachine) {
+        this.gumballMachine = gumballMachine;
+    }
+
+    public void insertQuarter() {
+        System.out.println("Please wait, your gumball is on its way");
     }
 
     public void ejectQuarter() {
-
+        System.out.println("Sorry, you already turned the crank");
     }
 
     public void turnCrank() {
-
+        System.out.println("Turning twice doesn't give you more gumballs");
     }
 
     public void dispense() {
-
+        gumballMachine.releaseBall();
+        if (gumballMachine.getCount() > 0)
+            gumballMachine.setState(gumballMachine.getNoQuarterState());
+        else {
+            System.out.println("Oops, out of gumballs");
+            gumballMachine.setState(gumballMachine.getSoldOutState());
+        }
     }
 }
