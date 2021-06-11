0161b43882a1a70e64ef14bea0fd500be49b67b9
Robert Rayburn <robert.rayburn@core10.io>
2021-06-10 23:16:37 -0500

    added small note about another route for this interface

--- Chapter10/State/src/State.java
+++ Chapter10/State/src/State.java
@@ -1,4 +1,7 @@
 public interface State {
+    // NOTE: If we want to prevent certain states from performing
+    //  any of these behaviors, we could initially make this an
+    //  abstract class and throw a NotImplementedException initially
     void insertQuarter();
     void ejectQuarter();
     void turnCrank();
