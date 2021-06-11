1700d1ef4eca27015a76ea20d6fb7a2926ed797e
Robert Rayburn <robert.rayburn@core10.io>
2021-06-10 22:46:47 -0500

    added bad design based on newly requested changes

--- Chapter10/README.md
+++ Chapter10/README.md
@@ -5,3 +5,11 @@
 ### Principles to know
 
 ### Bullet Points
+
+Q: Which of the following describe the state of our implementation?
+- [x] This code certainly isn’t adhering to the Open Closed Principle.
+- [x] This code would make a FORTRAN programmer proud.
+- [x] This design isn’t even very object-oriented.
+- [x] State transitions aren’t explicit; they are buried in the middle of a bunch of conditional statements.
+- [x] We haven’t encapsulated anything that varies here.
+- [x] Further additions are likely to cause bugs in working code.
--- Chapter10/State/src/GumballMachine.java
+++ Chapter10/State/src/GumballMachine.java
@@ -1,9 +1,12 @@
+import java.util.Random;
+
 public class GumballMachine {
 
     final static int SOLD_OUT = 0;
     final static int NO_QUARTER = 1;
     final static int HAS_QUARTER = 2;
     final static int SOLD = 3;
+    final static int WINNER = 4;
 
     int state = SOLD_OUT;
     int count = 0;
@@ -21,6 +24,7 @@ public class GumballMachine {
         }
         else if (state == SOLD_OUT) System.out.println("You can't insert a quarter, the machine is sold out.");
         else if (state == SOLD) System.out.println("Please wait, your gumball is on its way.");
+        else if (state == WINNER) System.out.println("Please wait, both your gumballs are on their way.");
     }
 
     public void ejectQuarter() {
@@ -29,30 +33,51 @@ public class GumballMachine {
             state = NO_QUARTER;
         }
         else if (state == NO_QUARTER) System.out.println("You haven't inserted a quarter");
-        else if (state == SOLD) System.out.println("Sorry, you already turned the crank");
+        else if (state == SOLD || state == WINNER) System.out.println("Sorry, you already turned the crank");
         else if (state == SOLD_OUT) System.out.println("You can't eject, you haven't inserted a quarter yet");
     }
 
     public void turnCrank() {
-        if (state == SOLD) System.out.println("Turning twice doesn't give you another gumball");
+        if (state == SOLD || state == WINNER) System.out.println("Turning twice doesn't give you another gumball");
         else if (state == NO_QUARTER) System.out.println("You turned but there is no quarter");
         else if (state == SOLD_OUT) System.out.println("You turned, but there are no gumballs");
         else if (state == HAS_QUARTER) {
             System.out.println("You turned...");
-            state = SOLD;
+            if (new Random().nextInt(10) == 1) {
+                System.out.println("You are a winner!");
+                state = WINNER;
+            }
+            else state = SOLD;
             dispense();
         }
     }
 
     public void dispense() {
         if (state == SOLD) {
-            System.out.println("Retrieve you gumball");
+            System.out.println("Retrieve your gumball");
             count--;
             if (count == 0) {
                 System.out.println("Oops, out of gumballs");
                 state = SOLD_OUT;
             }
             else state = NO_QUARTER;
+        }
+        else if (state == WINNER) {
+            System.out.println("Retrieve your gumball");
+            count--;
+            if (count == 0) {
+                System.out.println("Sorry, that was the last gumball!");
+                state = SOLD_OUT;
+            }
+            else {
+                System.out.println("Here is your extra gumball!");
+                count--;
+                if (count == 0) {
+                    System.out.println("Oops, out of gumballs");
+                    state = SOLD_OUT;
+                }
+                else state = NO_QUARTER;
+            }
         }
         else if (state == NO_QUARTER) System.out.println("You need to pay first");
         else if (state == SOLD_OUT) System.out.println("No gumball dispensed");
@@ -67,13 +92,14 @@ public class GumballMachine {
     public String toString() {
         StringBuilder builder = new StringBuilder();
         builder.append("\n\"Gumballs-2-Go\"\n")
-                .append("Java-enabled standing Model #2021-1\n")
+                .append("Java-enabled standing Model #2021-2\n")
                 .append("Inventory: ").append(count).append("\n");
 
         if (state == SOLD_OUT) builder.append("Machine is sold out\n");
         else if (state == NO_QUARTER) builder.append("Machine is waiting for your quarter\n");
         else if (state == HAS_QUARTER) builder.append("Machine is waiting to be cranked\n");
         else if (state == SOLD) builder.append("Machine is giving you your gumball\n");
+        else if (state == WINNER) builder.append("You are a winner, you get two gumballs\n");
         else builder.append("UNKNOWN_STATE\n");
         return builder.toString();
     }
