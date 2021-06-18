12a9f159ff1f6bbf392964f0ba085d30a082ee37
Robert Rayburn <robert.rayburn@core10.io>
2021-06-18 14:26:42 -0500

    added example of usign java rmi for acessing service

---
+++ Chapter11/Proxy/src/ProxyExample/MyRemoteImpl.java
@@ -0,0 +1,22 @@
+package ProxyExample;
+
+import java.rmi.Naming;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+public class MyRemoteImpl extends UnicastRemoteObject implements MyRemote {
+    protected MyRemoteImpl() throws RemoteException { }
+
+    public String saysHello() throws RemoteException {
+        return "Server says hello!";
+    }
+
+    public static void main(String[] args) {
+        try {
+            MyRemote service = new MyRemoteImpl();
+            Naming.rebind("RemoteHello", service);
+        } catch(Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+}
