ecbe4ccd82c7e7f07247479aedf19c2c74abb96b
Robert Rayburn <robert.rayburn@core10.io>
2021-06-24 23:18:43 -0500

    added duck factories to compound pattern example

---
+++ Chapter12/Compound/src/Factories/DuckFactory.java
@@ -0,0 +1,22 @@
+package Factories;
+
+import Base.*;
+
+public class DuckFactory extends AbstractDuckFactory {
+
+    public Quackable createMallardDuck() {
+        return new MallardDuck();
+    }
+
+    public Quackable createRedheadDuck() {
+        return new RedheadDuck();
+    }
+
+    public Quackable createDuckCall() {
+        return new DuckCall();
+    }
+
+    public Quackable createRubberDuck() {
+        return new RubberDuck();
+    }
+}
